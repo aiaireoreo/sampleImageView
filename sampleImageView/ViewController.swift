57c1e17240716e586e77bfdbbfb942c6ff9e62dd
AI Matsubara <aiai.1116.info@gmail.com>
2016-05-02 20:54:27 +0800

    initial commit

--- sampleImageView/ViewController.swift
+++ sampleImageView/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        myImageView.image =
+        UIImage(named:"sample.png")
+        
     }
 
     override func didReceiveMemoryWarning() {
